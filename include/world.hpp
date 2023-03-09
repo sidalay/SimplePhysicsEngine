40432d80c32fdf84f20439a8da2b0a4d9449297d
Sid Shingaki <sidalaytv@gmail.com>
2023-03-09 03:22:30 -0800

    chore: world.hpp
    - add array to count each shapes instances

--- include/world.hpp
+++ include/world.hpp
@@ -2,6 +2,7 @@
 #define WORLD_HPP
 
 #include <vector>
+#include <array>
 
 #include "object.hpp"
 
@@ -17,7 +18,8 @@ namespace spe
     
   private:
     float               m_gravity{};
-    int                 m_instances{};
+    int                 m_total{};
+    std::array<int, 3>  m_count{};
     std::vector<Object> m_objects{};
     raylib::Vector2     m_world{600.f, 600.f};
   };
