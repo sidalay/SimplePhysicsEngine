7191a8e5e18efc699415bc0df45c2eceee3bba6e
Sid Shingaki <sidalaytv@gmail.com>
2023-03-09 03:16:09 -0800

    initial layout

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include "world.hpp"
+
+int main(void)
+{
+  // Initialization
+  raylib::Vector2 window{800.f, 600.f};
+
+  InitWindow(window.x, window.y, "Simple Physics Engine");
+  SetTraceLogLevel(LOG_NONE);
+  SetTargetFPS(144);
+
+  // Main loop
+  while (!WindowShouldClose())
+  {
+    // Update
+    
+    // Draw
+    BeginDrawing();
+
+    ClearBackground(BLACK);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
