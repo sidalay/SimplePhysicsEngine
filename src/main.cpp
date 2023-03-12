db6577fa07e27a4c27013679a64e72e854ac3671
Sid Shingaki <sidalaytv@gmail.com>
2023-03-11 23:22:39 -0800

    chore: update tests

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main(void)
   // Initialization
   raylib::Vector2 window{800.f, 600.f};
   spe::Gaia gaia{};
+  gaia.LoadObject(spe::Object{0, Shape::SQUARE, {80.f,80.f}, {20.f, 20.f}});
 
   InitWindow(window.x, window.y, "Simple Physics Engine");
   SetTraceLogLevel(LOG_NONE);
@@ -17,6 +18,8 @@ int main(void)
     
     // Draw
     BeginDrawing();
+
+    gaia.DrawObjects();
 
     ClearBackground(BLACK);
     EndDrawing();
