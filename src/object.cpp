1591aca7a689aafd0e6a676c860a5e60109ed110
Sid Shingaki <sidalaytv@gmail.com>
2023-03-11 23:22:01 -0800

    chore: basic Draw() impl

--- src/object.cpp
+++ src/object.cpp
@@ -6,17 +6,35 @@ namespace spe
   Object::Object(
     const uint32_t id, 
     const Shape shape, 
-    raylib::Vector2 pos
+    raylib::Vector2 pos,
+    raylib::Vector2 size
   ) 
-  : m_shape{shape}, m_pos{pos}, m_id{id} {
+  : m_id{id}, m_shape{shape}, m_pos{pos}, m_size{size} {
     
   }
 
   void Object::Draw() const {
-
+    switch (m_shape)
+    {
+      case Shape::SQUARE:
+        DrawRectangleLines(m_pos.x, m_pos.y, m_size.x, m_size.y, WHITE);
+        break;
+      case Shape::CIRCLE:
+        break;
+      case Shape::TRIANGLE:
+        break;
+    };
   }
 
   void Object::CheckCollision() {
-    
+    switch (m_shape)
+    {
+      case Shape::SQUARE:
+        break;
+      case Shape::CIRCLE:
+        break;
+      case Shape::TRIANGLE:
+        break;
+    };
   }
 }
