1a0e19bc8912df63fe9de870512dc23532954a76
Sid Shingaki <sidalaytv@gmail.com>
2023-03-13 00:51:03 -0700

    chore: update world construction

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include "world.hpp"
 
-int main(void)
+int main()
 {
   // Initialization
   raylib::Vector2 window{800.f, 600.f};
   spe::Gaia gaia{};
-  gaia.LoadObject(spe::Object{0, Shape::SQUARE, {80.f,80.f}, {20.f, 20.f}});
+  gaia.LoadObject(spe::Object{0, Shape::SQUARE, RigidBody::DYNAMIC, {80.f,80.f}, {20.f, 20.f}});
 
   InitWindow(window.x, window.y, "Simple Physics Engine");
   SetTraceLogLevel(LOG_NONE);
