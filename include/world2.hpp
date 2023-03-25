dcfb1e9ef8e68ec16013b7e678d5e8f07adfde6e
Sid Shingaki <sidalaytv@gmail.com>
2023-03-25 16:33:28 -0700

    refactor (WIP): world does not need to be a class

---
+++ include/world2.hpp
@@ -0,0 +1,30 @@
+#ifndef WORLD2_HPP
+#define WORLD2_HPP
+
+#include <vector>
+#include <array>
+#include <algorithm>
+
+#include "object.hpp"
+
+namespace spe 
+{
+  namespace world
+  {
+    struct Info
+    {
+
+    };
+
+    struct Objects
+    {
+
+    };
+
+    void Run();
+    void Initialize();
+    void Tick();
+  }
+}
+
+#endif // WORLD2_HPP
