6e9959c8d51e117f1df4c066a2f43167c6fcd1d6
Sid Shingaki <sidalaytv@gmail.com>
2023-04-02 20:27:03 -0700

    chore: add Cell functions
    - UpdateCells()
    - LoadCell()
    - CheckCell()

--- include/world.hpp
+++ include/world.hpp
@@ -2,6 +2,7 @@
 #define WORLD_HPP
 
 #include <string>
+#include <memory>
 
 #include "object.hpp"
 
@@ -13,17 +14,18 @@ namespace spe
     {
       uint16_t              id{};
       raylib::Rectangle     area{};
+      std::vector<std::unique_ptr<Object>>  objects;
     };
     
     struct Info
     {
-      float                 deltaTime{};
       uint32_t              instances{};
       std::vector<Object>   objects{};
     };
 
     struct Properties
     {
+      float                 deltaTime{};
       raylib::Vector2       dimensions{1920.f, 1080.f};
       raylib::Vector2       gRowCol{20.f, 10.f};
       std::vector<Cell>     grid{};
@@ -33,7 +35,7 @@ namespace spe
     void Run();
     void Initialize(Properties&, const std::string&&);
     void Tick(Info&, Properties&);
-    void LoadObject(Info&, spe::Object&&);
+    void LoadObject(Info&, Properties&, spe::Object&&);
     void UnloadObject(Info&, Properties&);
     void DrawObjects(const Info&);
     void TickObjects(Info&);
@@ -41,6 +43,9 @@ namespace spe
     void CheckCollisions(Info&);
     void ImpulseResolution(const spe::Object&, const spe::Object&);
     void InitializeGrid(Properties&);
+    void UpdateCells(const Info&, Properties&);
+    void LoadCell(Cell&, spe::Object&);
+    bool CheckCell(Cell&, spe::Object&);
     bool CheckBounds(const spe::Object&, const Properties&);
   }
 }
